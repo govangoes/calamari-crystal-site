594266d09b65e6df37473a6a97698577e3db2fed
govangoes <govangoes@gmail.com>
2025-08-31 08:02:11 -0400

    test: mock IntersectionObserver; guard ScrollReveal in non-browser envs

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,18 @@
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom/vitest'
+import { afterEach, vi } from 'vitest'
+import { cleanup } from '@testing-library/react'
+
+afterEach(() => cleanup())
+
+// --- Mock IntersectionObserver for jsdom ---
+if (typeof global.IntersectionObserver === 'undefined') {
+  class IO {
+    constructor() {}
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+    takeRecords() { return [] }
+  }
+  // @ts-ignore
+  global.IntersectionObserver = IO
+}
