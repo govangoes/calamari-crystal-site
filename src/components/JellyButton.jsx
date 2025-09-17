e4ed4e59bc4c9a85d7867da899b86acda24ee4ae
Evan Figueroa <govangoes@gmail.com>
2025-09-17 18:13:18 -0400

    feat: add JellyButton component from Uiverse.io

---
+++ src/components/JellyButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './JellyButton.css';
+
+const JellyButton = ({ children = "Jelly Button", onClick, className = "", ...props }) => {
+  return (
+    <button 
+      className={`Btn ${className}`}
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default JellyButton;
